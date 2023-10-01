f29208d3a772baef2c5db28f750a65b7f618b203
Muhammad Bhutta <bhutta@iza.org>
2023-10-01 05:14:52 +0200

    added

--- routes.py
+++ routes.py
@@ -29,6 +29,9 @@ GOOGLE_MAPS_API_KEY = "AIzaSyDiO4rSQtabv-kWnToMjUbbnKxAKL2b0-I"
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+@app.route('/', methods=['GET'])
+def hello():
+    return jsonify({"intro":"hello"}), 201
 
 @app.route('/login', methods=['POST'])
 def login():
